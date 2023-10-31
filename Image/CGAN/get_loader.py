408fa94e799c592f3adf4e65c709acd3fb3778e0
Maiol <132783746+MaiolSabater@users.noreply.github.com>
2023-10-31 18:54:43 +0100

    Create get_loader.py

---
+++ Image/CGAN/get_loader.py
@@ -0,0 +1,18 @@
+import torchvision.transforms as transforms
+import torch
+import torchvision
+
+def get_loader():
+  transform=transforms.Compose([
+                                transforms.Resize(image_size),
+                                transforms.CenterCrop(image_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)),
+                            ])
+
+  train_set = torchvision.datasets.FashionMNIST(
+      root=".", train = True, download=True, transform=transform
+  )
+  dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                          shuffle=True)
+  return dataloader
