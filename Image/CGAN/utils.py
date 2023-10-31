7a8847b8730ad0a98c03297b3f358df11d11a882
Maiol <132783746+MaiolSabater@users.noreply.github.com>
2023-10-31 18:53:43 +0100

    Create utils.py

---
+++ Image/CGAN/utils.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+def plot_losses(G_losses,D_losses):
+    plt.figure(figsize=(10,5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(G_losses,label="G")
+    plt.plot(D_losses,label="D")
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+def animations(img_list):
+    fig = plt.figure(figsize=(8,8))
+    plt.axis("off")
+    ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+    HTML(ani.to_jshtml())
